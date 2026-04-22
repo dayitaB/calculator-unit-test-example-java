d56db01e6046a500748d240da6fcdff9a1b7117d
dayitaB <dayanna.bravo@epn.edu.ec>
2026-04-22 13:00:02 -0500

    Creación de pruebas unitarias para los métodos de resta, multiplicación y división.

--- src/test/java/ec/edu/epn/calculator/CalculatorTest.java
+++ src/test/java/ec/edu/epn/calculator/CalculatorTest.java
@@ -1,18 +1,53 @@
 package ec.edu.epn.calculator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.Assert;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void testSum(){
-        Calculator calculator = new Calculator();
         int result = calculator.sum(2, 2);
         if(result!=4){
             Assert.fail();
         }
     }
-    
+
+    @Test
+    public void testMinus(){
+        int result = calculator.minus(2, 1);
+        if(result!=1){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testMultiply(){
+        int result = calculator.multiply(2, 3);
+        if(result!=6){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testDivideSuccess(){
+        int result = calculator.divide(6, 3);
+        if(result!=2){
+            Assert.fail();
+        }
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideFail(){
+        calculator.divide(6, 0);
+    }
 }
